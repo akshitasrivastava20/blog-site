1a1d2f034da5a5bfb488b821cf80d6ec59c22943
Your Name <akshita.srivastava.1505@gmail.com>
2025-05-30 23:12:51 +0530

    blogiy

---
+++ models/user.js
@@ -0,0 +1,42 @@
+const {Schema,model}=require('mongoose');
+
+const userSchema=new Schema({
+    fullName:{
+        type:String,
+        required:true,
+        unique:true,
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true,
+    },
+    salt:{
+        type:String,
+        required:true, 
+    },
+    password:{
+        type:String,
+        required:true,
+    },
+    profileImageUrl:{
+        type:String,
+        default:"/images/default.png",
+    },
+    role:{
+        type:String,
+        enum:["USER","ADMIN"],
+        default:"USER",
+    },
+
+},{timestamps:true});
+
+userSchema.pre("save",function(next){
+    const user=this;
+
+    if(!user.isModified("password")) return;
+    
+})
+
+const User=model('user',userSchema);
+module.exports=User
